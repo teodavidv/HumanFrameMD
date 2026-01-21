5c75f05ce7033d95d4ff3f4b7959f12eb1469fdf
teodavidv <teo@amhventures.com>
2026-01-20 18:20:23 -0800

    Delete SmoothSpine_files directory

--- SmoothSpine_files/10041878-10049715.js
+++
@@ -1,1 +0,0 @@
-(function(){return {"error":true,"code":"wrongConfig-dataError"};})()
--- SmoothSpine_files/loader.js
+++
@@ -1,4 +0,0 @@
-(function() {
-  const n = document.currentScript;
-  n != null && n.src ? import(new URL("kaching-bundles.js", n.src).href) : import("./kaching-bundles.js");
-})();
